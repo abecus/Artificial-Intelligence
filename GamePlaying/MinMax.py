e34353b38a561b9c5841e9805e702e4d43622a4e
abecus <insaaone@gmail.com>
2019-06-24 16:00:12 +0530

    !

--- GamePlaying/MinMax.py
+++ GamePlaying/MinMax.py
@@ -8,8 +8,10 @@ class MinMax():
 
     def __init__(self, leaf_values, tree_matrix, *args, **kwargs):
         '''
-        tree_matrix : no of rows corresponds to the depths of the game-tree and elements corresponds
+        * tree_matrix : no of rows corresponds to the depths of the game-tree and elements corresponds
         to the branching of the nodes at that depth (eg. 3 can be seen as at that node 3 branches are comming)
+
+        * leaf_values : values (scores) at leaf
         '''
         self.leaf_values = leaf_values
         self.tree_matrix = tree_matrix
@@ -19,10 +21,16 @@ class MinMax():
     @staticmethod
     def compute_row(turn, values, row_of_tree_matrix):
 
+        '''
+        computes values a level down in the tree towards root
+        '''
+
         val = np.array([])
+        # ab_tracker = np.array([])
         track = 0
 
         if turn == 'min':
+
             for branching in row_of_tree_matrix:
                 if branching != 0: 
 
@@ -46,6 +54,10 @@ class MinMax():
 
     def run(self):
 
+        '''
+        runs the function compute_row on all tree
+        '''
+
         turn = 'min' if self.depth % 2 == 0  else 'max'
         value = self.leaf_values
 
@@ -61,9 +73,9 @@ class MinMax():
 #%%
 if __name__ == "__main__":
     
-    leaf = np.array([2, 3, 4, 5, 6])
+    leaf = np.array([2, 3, 4, 5, 6, 7])
     tree = np.array([[3, 0, 0],
-            [2, 0, 3]])
+            [2, 1, 3]])
 
     game = MinMax(leaf, tree)
     game.run()
